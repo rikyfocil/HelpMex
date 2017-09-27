18cc0eabf62d0a4ca4225377c66d4931a2a5f4a6
Ricardo López <rikyfocil@Ricardos-MacBook-Pro.local>
2017-09-27 12:54:28 -0500

    Credado semaforo <3

--- resources/views/donador/index.blade.php
+++ resources/views/donador/index.blade.php
@@ -1,82 +1,53 @@
 @extends('donador.sidebar')
 @section('content')
+
 	<div id="fh5co-main">
 
 		<div class="fh5co-narrow-content">
 			<h2 class="fh5co-heading animate-box" data-animate-effect="fadeInLeft">We Love To Design <span>See Our Portfolio</span></h2>
-			<div class="row animate-box" data-animate-effect="fadeInLeft">
-				<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 work-item">
-					<a href="work.html">
-						<img src="images/work_1.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3 class="fh5co-work-title">Work Title Here</h3>
-						<p>Illustration, Branding</p>
-					</a>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 work-item">
-					<a href="work.html">
-						<img src="images/work_2.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3 class="fh5co-work-title">Work Title Here</h3>
-						<p>Web, Packaging</p>
-					</a>
-				</div>
-				<div class="clearfix visible-sm-block"></div>
-				<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 work-item">
-					<a href="work.html">
-						<img src="images/work_3.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3 class="fh5co-work-title">Work Title Here</h3>
-						<p>Branding, Web</p>
-					</a>
-				</div>
-				<div class="clearfix visible-md-block"></div>
-				<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 work-item">
-					<a href="work.html">
-						<img src="images/work_4.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3 class="fh5co-work-title">Work Title Here</h3>
-						<p>Logo, Branding, Web</p>
-					</a>
-				</div>
-				<div class="clearfix visible-sm-block"></div>
-				<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 work-item">
-					<a href="work.html">
-						<img src="images/work_5.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3 class="fh5co-work-title">Work Title Here</h3>
-						<p>Web, Packaging, Branding</p>
-					</a>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 work-item">
-					<a href="work.html">
-						<img src="images/work_6.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3 class="fh5co-work-title">Work Title Here</h3>
-						<p>Branding</p>
-					</a>
-				</div>
-				<div class="clearfix visible-md-block visible-sm-block"></div>
 
-				<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 work-item">
-					<a href="work.html">
-						<img src="images/work_7.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3 class="fh5co-work-title">Work Title Here</h3>
-						<p>Web, Illustration</p>
+			<h3> Urgencia Alta </h3>
+
+			<div class="row animate-box" data-animate-effect="fadeInLeft">
+				
+				<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 work-item urgency-high">
+					<a>
+						<p><i class="fa fa-5x fa-pencil" aria-hidden="true"></i><p> <br>
+						<h3 class="fh5co-work-title">Enlatados</h3>
+						<p>Sub1 </p>
 					</a>
 				</div>
-				<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 work-item">
-					<a href="work.html">
-						<img src="images/work_8.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3 class="fh5co-work-title">Work Title Here</h3>
-						<p>Branding, Web</p>
-					</a>
-				</div>
-				<div class="clearfix visible-sm-block"></div>
-				<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 work-item">
-					<a href="work.html">
-						<img src="images/work_1.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
-						<h3 class="fh5co-work-title">Work Title Here</h3>
-						<p>Illustration, Branding</p>
-					</a>
-				</div>
-				<div class="clearfix visible-md-block"></div>
 				
 			</div>
+
+			<h3> Urgencia Media </h3>
+
+			<div class="row animate-box" data-animate-effect="fadeInLeft">
+				
+				<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 work-item urgency-med">
+					<a>
+						<p><i class="fa fa-5x fa-pencil" aria-hidden="true"></i><p> <br>
+						<h3 class="fh5co-work-title">Enlatados</h3>
+						<p>Sub1 </p>
+					</a>
+				</div>
+				
+			</div>
+
+			<h3> Urgencia Baja </h3>
+
+			<div class="row animate-box" data-animate-effect="fadeInLeft">
+				
+				<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 work-item urgency-low">
+					<a>
+						<p><i class="fa fa-5x fa-pencil" aria-hidden="true"></i><p> <br>
+						<h3 class="fh5co-work-title">Enlatados</h3>
+						<p>Sub1 </p>
+					</a>
+				</div>
+				
+			</div>
+
 		</div>
 		
 		
